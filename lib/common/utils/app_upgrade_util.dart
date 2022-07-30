2ef62712292b0a856b639bfd2571dd5b678af057
何宗杰 <zongjie@kooboo.net>
2022-07-30 16:39:29 +0800

    update kooboo_flutter_app_upgrade version

--- lib/common/utils/app_upgrade_util.dart
+++ lib/common/utils/app_upgrade_util.dart
@@ -2,15 +2,9 @@ import 'package:kooboo_flutter_app_upgrade/flutter_app_upgrade.dart';
 
 ///应用内升级
 class AppUpgradeKit {
-  ///获取apk 下载路径(仅适用于Android)
-  static Future<String> get apkDownloadPath {
-    return FlutterAppUpgrade.apkDownloadPath;
-  }
-
-  ///安装apk(仅适用于Android)
-  ///[path]: 'apkDownloadPath/downloadApkName';
-  static installAppForAndroid(String path) {
-    return FlutterAppUpgrade.installAppForAndroid(path);
+  ///下载并安装apk(仅适用于Android)
+  static downloadApkInstall(String downloadUrl, String versionName) async {
+    return FlutterAppUpgrade.downloadApkInstall(downloadUrl, versionName);
   }
 
   ///跳转AppStore(仅适用于iOS)
